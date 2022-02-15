49d59adb36de05dab8049aa3ff0b9efa74757200
shashi gharti <shashi.gharti@gmail.com>
2022-02-15 21:12:03 +0545

    update env sample files and code refactored

--- client/app.js
+++ client/app.js
@@ -7,10 +7,10 @@ const App = () => {
     <>
       <Router>
         <Routes>
-          <Route exact path='/' element={<MainPage />} />
-          <Route exact path='/admin' element={<AdminPage />} />
-          <Route exact path='/book-client' element={<BookClientPage />} />
-          <Route exact path='/ui-client' element={<UIClientPage />} />
+          <Route path='/' element={<MainPage />} />
+          <Route path='/admin' element={<AdminPage />} />
+          <Route path='/book-client' element={<BookClientPage />} />
+          <Route path='/ui-client' element={<UIClientPage />} />
         </Routes>
       </Router>
     </>
